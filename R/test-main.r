890c471c7d39b98a5dcca51d9ae168af00a8f476
hrbrmstr <bob@rud.is>
2017-11-11 21:18:38 -0500

    temp removal of 'best' from spd_test() due to weird pingr behaviour on ubuntu

--- R/test-main.r
+++ R/test-main.r
@@ -22,19 +22,20 @@ spd_test <- function() {
 
   cat(cyan("Determining best server...\n"))
   servers <- spd_closest_servers(servers, config)
-  best <- spd_best_servers(servers, config, max=3)
+  best <- servers
+#  best <- spd_best_servers(servers, config, max=3)
 
   cat(green("Initiating test from ") %+% white(config$client$isp) %+% green(" (") %+%
         white(config$client$ip) %+% green(") to ") %+% white(best$sponsor[1]) %+%
         green(" (") %+% white(best$name[1]) %+% green(")\n\n"))
 
   cat(white$bold("Analyzing download speed"))
-  down <- spd_download_test(best, config, FALSE, .progress="dots")
+  down <- spd_download_test(best, config, FALSE, timeout=5, .progress="dots")
 
   cat(green("Download: ") %+% white$bold(as.integer(max(down$bw))) %+% white$bold(" Mbit/s\n"))
 
   cat(white$bold("\nAnalyzing upload speed"))
-  up <- spd_upload_test(best, config, FALSE, .progress="dots")
+  up <- spd_upload_test(best, config, FALSE, timeout=10, .progress="dots")
 
   cat(green("Upload: ") %+% white$bold(as.integer(max(up$bw))) %+% white$bold(" Mbit/s\n"))
 
